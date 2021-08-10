2c7ecf8ca020e85c73a157f4523621fd0ea9a489
riderx <martindonadieu@gmail.com>
2021-08-10 15:01:13 +0200

    fix: deploy with main branch

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:riderx/autoswitch-linkedin-entreprise.git master:gh-pages
-
+git push -f git@github.com:riderx/autoswitch-linkedin-entreprise.git main:gh-pages
+#        https://github.com/riderx/autoswitch-linkedin-entreprise
 cd -
